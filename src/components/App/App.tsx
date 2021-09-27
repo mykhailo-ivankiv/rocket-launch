f8c84f3b8057b5190fc7aaed9154b9b009f217f4
mivankiv <mivankiv@degiro.nl>
2021-09-27 20:42:31 +0200

    Update styles a bit

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,11 @@ import './App.css';
 import {useAsync} from "react-use";
 import {memoizeWith, map} from "ramda";
 import {jsonToQueryString} from "../utils/data";
+// styles
+import BEM from "../utils/BEM";
+import "./App.css"
+const b = BEM("App")
+
 
 const getUrl = (baseUrl: string, params: Object): string => {
     const queryString = jsonToQueryString(params)
@@ -14,32 +19,55 @@ const get = memoizeWith(
     (url: string, params: Object = {}): string => getUrl(url, params),
     async (url: string, params: Object) => await (await fetch(getUrl(url, params))).json())
 
+type NumberInterval = { start: number, end: number }
+type DateInterval = { start: Date, end: Date }
+type Interval = NumberInterval | DateInterval
 
 type LaunchRequestParams = {}
+type LaunchResponse = {
+    results: {
+        name: string,
+        slug: string,
+        window_start: string
+        window_end: string
+    }[]
+}
 type Launch = {
     name: string,
-    slug: string
-}
-type LaunchResponse = {
-    results: Launch[]
+    slug: string,
+    window: DateInterval
 }
 
+const upcomingLunchesAdapter = (rawData: LaunchResponse): Launch[] => map(({name, slug, window_start, window_end}) => ({
+    name,
+    slug,
+    window: {
+        start: new Date(window_start),
+        end: new Date(window_end)
+    }
+}), rawData.results)
 
-const getLunches = async (params: LaunchRequestParams): Promise<LaunchResponse> => get("https://lldev.thespacedevs.com/2.2.0/launch", params)
+// const getLunches = async (params: LaunchRequestParams): Promise<LaunchResponse> => get("https://lldev.thespacedevs.com/2.2.0/launch", params)
+
+// @ts-ignore
+const getUpcomingLunches = async (params: LaunchRequestParams): Promise<Launch> => upcomingLunchesAdapter(
+    (await get("https://lldev.thespacedevs.com/2.2.0/launch/upcoming", params) as LaunchResponse)
+)
 
 
-function App() {
-    const [lunchesParams, setLunchesRequestParams] = useState<LaunchRequestParams>({});
-    const launch = useAsync(() => getLunches(lunchesParams), [lunchesParams])
+const App = () => {
+    const [lunchesParams] = useState<LaunchRequestParams>({});
+    const launch = useAsync(() => getUpcomingLunches(lunchesParams), [lunchesParams])
 
 
     return (
-        <div className="App">
+        <div className={b()}>
             {launch.loading && launch.value && "Loading..."}
+            {launch.error && <div>Error: {JSON.stringify(launch.error)}</div>}
             {/*// @ts-ignore*/}
-            {map(({name, slug}: object) => <div key={slug}>{name}</div>, launch.value?.results || [])}
+            {map(({name, slug}: Launch) => <div key={slug}>{name}</div>, launch.value || [])}
         </div>
     );
-}
+};
 
 export default App;
