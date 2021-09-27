3a823051de94d362b6f208a7b5c58fc8f44e600d
mivankiv <mivankiv@degiro.nl>
2021-09-27 22:02:42 +0200

    Out of time

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,10 @@
 import React, {useState} from 'react';
-import {useAsync} from "react-use";
+import {useAsync, useDebounce} from "react-use";
 import {memoizeWith, map} from "ramda";
 import {jsonToQueryString} from "../utils/data";
 // styles
 import BEM from "../utils/BEM";
 import "./App.css"
-import Time from "../Time/Time";
 import {DateIntervalType} from "../utils/commonTypes";
 import Launch from "../Launch/Launch";
 
@@ -15,20 +14,23 @@ const b = BEM("App")
 const getUrl = (baseUrl: string, params: Object): string => {
     const queryString = jsonToQueryString(params)
 
-    return `${baseUrl}${queryString ? ` ?${queryString}` : ""}`;
+    return `${baseUrl}${queryString ? `?${queryString}` : ""}`;
 }
 
 const get = memoizeWith(
     (url: string, params: Object = {}): string => getUrl(url, params),
     async (url: string, params: Object) => await (await fetch(getUrl(url, params))).json())
 
-type LaunchRequestParams = {}
+type LaunchRequestParams = {
+    search?: string
+}
 type LaunchResponse = {
     results: {
         name: string,
         slug: string,
         window_start: string
         window_end: string
+        location__id: number,
     }[]
 }
 type LaunchType = {
@@ -37,19 +39,16 @@ type LaunchType = {
     window: DateIntervalType
 }
 
-const upcomingLunchesAdapter = (rawData: LaunchResponse): LaunchType[] => map(({
-                                                                                   name,
-                                                                                   slug,
-                                                                                   window_start,
-                                                                                   window_end
-                                                                               }) => ({
-    name,
-    slug,
-    window: {
-        start: new Date(window_start),
-        end: new Date(window_end)
-    }
-}), rawData.results)
+const upcomingLunchesAdapter = (rawData: LaunchResponse): LaunchType[] =>
+    map(({name, slug, window_start, window_end, location__id}) => ({
+        name,
+        slug,
+        window: {
+            start: new Date(window_start),
+            end: new Date(window_end)
+        },
+        locationId: location__id,
+    }), rawData.results)
 
 const getLunches = async (params: LaunchRequestParams): Promise<LaunchResponse> => get("https://lldev.thespacedevs.com/2.2.0/launch", params)
 
@@ -60,16 +59,27 @@ const getUpcomingLunches = async (params: LaunchRequestParams): Promise<LaunchTy
 
 
 const App = () => {
-    const [lunchesParams] = useState<LaunchRequestParams>({});
+    const [lunchesParams, setLunchesParams] = useState<LaunchRequestParams>({});
     const launch = useAsync(() => getUpcomingLunches(lunchesParams), [lunchesParams])
+    const [search, setSearch] = useState<string>("");
+
+    useDebounce(() => setLunchesParams((lunchesParams) => ({...lunchesParams, search})), 400, [search]);
 
     return (
         <div className={b()}>
-            {launch.loading && launch.value && "Loading..."}
+            <input className={b("search-input")} type="text" value={search}
+                   onChange={({target}) => setSearch(target.value.trim())}/>
+            {launch.loading && !launch.value && <div>Loading...</div>}
             {launch.error && <div>Error: {JSON.stringify(launch.error)}</div>}
-            {/*// @ts-ignore*/}
-            {map(({name, slug, window}: LaunchType) => <Launch name={name} slug={slug}
-                                                               window={window}/>, launch.value || [])}
+
+            <div className={b("result", {loading: launch.loading})}>
+                {// @ts-ignore*/
+                    launch.value?.length !== 0
+                        // @ts-ignore*/
+                        ? map(({name, slug, window}: LaunchType) => <Launch name={name} window={window}/>, launch.value || [])
+                        : <span>"No data"</span>}
+            </div>
+
         </div>
     );
 };
--- src/components/utils/data.ts
+++ src/components/utils/data.ts
@@ -1,10 +1,10 @@
 import {pipe, reduce} from "ramda";
 
-export const jsonToQueryString = pipe(
+export const jsonToQueryString =  (params: object) => pipe(
     (obj: Record<string, any>) => Object.entries(obj),
     reduce((acc: URLSearchParams, [key, value]) => {
         acc.append(key, String(value))
         return acc;
     }, new URLSearchParams()),
     (params: URLSearchParams) => params.toString(),
-);
+)(params);
